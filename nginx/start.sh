4b4db6620acd7ac02460f5aaf476902a35da2232
abd-ashraf <abdaman7x@gmail.com>
2024-12-26 15:17:44 +0200

    updated start.sh script to close containers on STOP btn

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
-
-# Start nginx in background and get its process ID
-nginx & # & makes it run in background
-nginx_pid=$! # $! stores PID of last background process
+# Start nginx in foreground
+nginx &
+nginx_pid=$!
 
 # Keep monitoring other containers
 while true; do
-    # Check if any service1 or service2 containers are still running
-    # >/dev/null -> Redirect output to nowhere
-    # >/dev/null -> Redirect output to nowhere
-    # 2>&1 -> Redirect stderr to stdout
+    # Check if any service1 replicas or service2 are running
     if ! curl -s service1-1:8199 >/dev/null 2>&1 && \
        ! curl -s service1-2:8199 >/dev/null 2>&1 && \
        ! curl -s service1-3:8199 >/dev/null 2>&1 && \
