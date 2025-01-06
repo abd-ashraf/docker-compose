2128f0b8cba85c1c0219a99b0484412d50fc6596
abd-ashraf <abdaman7x@gmail.com>
2025-01-06 17:27:01 +0200

    added auth in service2 tests

--- tests/test_services/test_service2.py
+++ tests/test_services/test_service2.py
@@ -1,19 +1,22 @@
 import pytest
 import requests
 
+# authentication
+auth = ('admin', 'admin123')
+
 def test_service2_data_in_response():
     """Test if service2's data is included in the response"""
-    response = requests.get("http://localhost:8198/service1")
+    response = requests.get("http://localhost:8198/service1", auth=auth)
     data = response.json()
     
     assert response.status_code == 200
     assert "Service2" in data
     service2_data = data["Service2"]
-    assert isinstance(service2_data, dict)  # Verify it's a dictionary
+    assert isinstance(service2_data, dict)
 
 def test_service2_data_structure():
     """Test if service2's data has the expected structure"""
-    response = requests.get("http://localhost:8198/service1")
+    response = requests.get("http://localhost:8198/service1", auth=auth)
     data = response.json()
     
     assert response.status_code == 200
